759d78fd6f7efbd8b23ee5161a8daf6e7cd80cf0
Administrator <jianglinming@gzmtr.com>
2017-06-01 16:41:52 +0800

    6.5.7暂时保存，切换到主支去取代码3。

---
+++ app/src/main/java/com/mzd/mtakem2/HbHistory.java
@@ -0,0 +1,85 @@
+package com.mzd.mtakem2;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by Administrator on 2017/6/1 0001.
+ */
+
+public class HbHistory extends SQLiteOpenHelper {
+
+    private static final String TAG = "HbHistory";
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "HbHistory.db";
+    private final static String TABLE_NAME = "hblist";
+
+    public HbHistory(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        Log.i(TAG, "数据库构造器");
+    }
+
+    @Override
+    public void onOpen(SQLiteDatabase db) {
+        super.onOpen(db);
+        Log.i(TAG, "数据库打开");
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE hblist (id INTEGER PRIMARY KEY AUTOINCREMENT,group_name TEXT,sender TEXT, content TEXT," +
+                " unpacked_time TEXT, notify_consuming INT,chatlist_consuming INT,chatwindow_consuming INT,hb_amount REAL)");
+        Log.i(TAG, "创建数据表");
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.i(TAG, "数据库更新");
+    }
+
+    //插入方法
+    public void insert(ContentValues values){
+        //获取SQLiteDatabase实例
+        SQLiteDatabase db = getWritableDatabase();
+        //插入数据库中
+        db.insert(TABLE_NAME, null, values);
+        db.close();
+    }
+
+    //查询方法
+    public Cursor query(){
+        SQLiteDatabase db = getReadableDatabase();
+        //获取Cursor
+        Cursor c = db.query(TABLE_NAME, null, null, null, null, null, null, null);
+        return c;
+
+    }
+
+    //根据唯一标识_id  来删除数据
+    public void delete(int id){
+        SQLiteDatabase db = getWritableDatabase();
+        db.delete(TABLE_NAME, "id=?", new String[]{String.valueOf(id)});
+        db.close();
+    }
+
+    //更新数据库的内容
+    public void update(ContentValues values, String whereClause, String[]whereArgs){
+        SQLiteDatabase db = getWritableDatabase();
+        db.update(TABLE_NAME, values, whereClause, whereArgs);
+        db.close();
+    }
+
+    /**
+     * 删除数据库
+     *
+     * @param context
+     * @return
+     */
+    public boolean deleteDatabase(Context context) {
+        return context.deleteDatabase(DATABASE_NAME);
+    }
+
+}
---
+++ app/src/main/java/com/mzd/mtakem2/utils/ComFunc.java
@@ -0,0 +1,61 @@
+package com.mzd.mtakem2.utils;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+
+import com.mzd.mtakem2.R;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
+
+/**
+ * Created by Administrator on 2017/6/1 0001.
+ */
+
+public class ComFunc {
+
+    /*
+        获得手机Mac码
+     */
+    public static String getMac() {
+        String macSerial = null;
+        String str = "";
+        try {
+            Process pp = Runtime.getRuntime().exec(
+                    "cat /sys/class/net/wlan0/address ");
+            InputStreamReader ir = new InputStreamReader(pp.getInputStream());
+            LineNumberReader input = new LineNumberReader(ir);
+            for (; null != str; ) {
+                str = input.readLine();
+                if (str != null) {
+                    macSerial = str.trim();// 去空格
+                    break;
+                }
+            }
+        } catch (IOException ex) {
+            // 赋予默认值
+            ex.printStackTrace();
+        }
+        return macSerial;
+    }
+
+    /**
+     * 2  * 获取版本号
+     * 3  * @return 当前应用的版本号
+     * 4
+     */
+    public static String getVersion(Context context) {
+        try {
+            PackageManager manager = context.getPackageManager();
+            PackageInfo info = manager.getPackageInfo(context.getPackageName(), 0);
+            String version = info.versionName;
+            return version;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return context.getString(R.string.can_not_find_version_name);
+        }
+    }
+
+}
